7fab0ad1693969e9a0c53717345e25c1b89fa87e
jacobrakestraw <jacob.rakestraw@healthtell.com>
2017-06-22 17:02:57 -0700

    updated app.py to return tweets to app.route('/')

--- application/app.py
+++ application/app.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+     render_template, flash
+
 
 app = Flask(__name__)
 
@@ -9,43 +11,29 @@ except ImportError:
     import simplejson as json
 
 # Import the necessary methods from "twitter" library
-from twitter import Twitter, OAuth, TwitterHTTPError, TwitterStream
+import oauth2 as oauth
 
 # Variables that contains the user credentials to access Twitter API
-ACCESS_TOKEN = '1238348797-5GmDO5kldGC5tB5A6r0knWePIaR0a3crRBjwwMp"'
+ACCESS_TOKEN = '1238348797-5GmDO5kldGC5tB5A6r0knWePIaR0a3crRBjwwMp'
 ACCESS_SECRET = 'Ob28F909WOIda5R45dXcWlVeBkbyCZWKcEaAGy1cFalHV'
 CONSUMER_KEY = 'o5p2TNGv19WkEfu324YInDlZT'
 CONSUMER_SECRET = 'JnrFeTU0OepvxkUmt2D5ajEm3wY6Mwvficf0lW1tdGQwg5i6eJ'
 
-oauth = OAuth(ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
+consumer = oauth.Consumer(key=CONSUMER_KEY, secret=CONSUMER_SECRET)
+access_token = oauth.Token(key=ACCESS_TOKEN, secret=ACCESS_SECRET)
+client = oauth.Client(consumer, access_token)
 
-# Initiate the connection to Twitter Streaming API
-twitter_stream = TwitterStream(auth=oauth)
+timeline_endpoint = "https://api.twitter.com/1.1/statuses/user_timeline.json"
+response, data = client.request(timeline_endpoint)
 
+tweets = json.loads(data)
+my_gen = (item for item in tweets if item[u'text'].find('you') > -1)
 
 @app.route('/')
 def home():
-
-
-    # Get a sample of the public data following through Twitter
-    iterator = twitter_stream.statuses.sample()
-
-    # Print each tweet in the stream to the screen
-    # Here we set it to stop after getting 1000 tweets.
-    # You don't have to set it to stop, but can continue running
-    # the Twitter API to collect data for days or even longer.
-    tweet_count = 1000
-    tweets = {}
-    for tweet in iterator:
-        tweet_count -= 1
-        # Twitter Python Tool wraps the data returned by Twitter
-        # as a TwitterDictResponse object.
-        # We convert it back to the JSON format to print/score
-        tweets += json.dumps(tweet)
-
-        # The command below will do pretty printing for JSON data, try it out
-        # print json.dumps(tweet, indent=4)
-
-        if tweet_count <= 0:
-            break
-    return json.dumps(tweets)
+    global my_gen
+    tweetaronis = []
+    for item in my_gen:
+        tweet = unicode(item[u'user'][u'screen_name'] + ': ' + item[u'text'] + '\n')
+        tweetaronis.append(tweet)
+    return render_template('home.html', entries=tweetaronis)
